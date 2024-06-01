d34eebbc4da2dcd0e94ac0a1b9b78172945d973a
orrin-temp <zummsg@gmail.com>
2024-06-01 16:24:35 +0530

    lower footer complete desktop

--- src/components/footer/footer division/LowerFooter.jsx
+++ src/components/footer/footer division/LowerFooter.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../Footer.css";
 import { TfiWorld } from "react-icons/tfi";
 import { IoIosArrowDown } from "react-icons/io";
 
+const nikeProducts = [
+  "Nike Adapt",
+  "Nike Air",
+  "Nike Air Force 1",
+  "Nike Air Max",
+  "Nike FlyEase",
+  "Nike Flyknit",
+  "Nike Flyleather",
+  "Nike Free",
+  "Nike Joyride",
+  "Nike Pegasus",
+  "Nike React",
+  "Nike Vaporfly",
+  "Nike Zoom Fly",
+  "Nike ZoomX",
+];
+
 function LowerFooter() {
+  const guideHover = useRef(null);
+
+  function handleMouseEnter() {
+    if (guideHover.current) {
+      guideHover.current.style.opacity = "1";
+      guideHover.current.style.pointerEvents = "auto";
+    }
+  }
+  function handleMouseLeave() {
+    if (guideHover.current) {
+      guideHover.current.style.opacity = "0";
+      guideHover.current.style.pointerEvents = "none";
+    }
+  }
+
   return (
     <div className="lowerfooter">
       <div className="footerBoxesContainer">
@@ -48,9 +80,16 @@ function LowerFooter() {
       <div className="footerLinerContainer">
         <ul>
           <li>&#169;2024 Nike2.0, Inc. All rights reserved</li>
-          <li className="footerguidesicon">
-            Guides
-            <IoIosArrowDown />
+          <li className="footerguidesicon" onMouseLeave={handleMouseLeave}>
+            <div className="guideList" ref={guideHover}>
+              {nikeProducts.map((item, index) => {
+                return <p className="guidelistitem">{item}</p>;
+              })}
+            </div>
+            <span onMouseEnter={handleMouseEnter} className="spanGuide">
+              Guides
+              <IoIosArrowDown />
+            </span>
           </li>
           <li className="footerHover">Terms of Sale</li>
           <li className="footerHover">Terms of Use</li>
