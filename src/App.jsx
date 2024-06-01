c1c90a0d139909c4d465c21a9cce515a8eb2f2e2
Abhradip Roy <abhra0312@gmail.com>
2024-06-01 13:26:06 +0530

    Navbar initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
+  let name="whoremanga"
   return (
-    <></>
+    <>
+    <Navbar name={name}/>
+    </>
   )
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar(obj) {
+  return (
+   <div></div>
+  )
+}
+
+export default Navbar
