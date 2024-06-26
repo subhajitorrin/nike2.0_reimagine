316f011cb9d58c683892789812b1ee7e1755427f
subhajitorrin <subhajitghosh343@gmail.com>
2024-06-27 01:35:21 +0530

    shoecard animaiton

--- src/components/CardSection/ShoeCard.jsx
+++ src/components/CardSection/ShoeCard.jsx
@@ -1,19 +1,61 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useRef } from "react";
 import { GoArrowRight } from "react-icons/go";
 
 function ShoeCard({ shoe, title, desc }) {
+  const shoeCardRef = useRef(null);
+  const circleCoverRef = useRef(null);
+  const arrowRef = useRef(null);
+  function handelHoverEnter() {
+    if (shoeCardRef.current && circleCoverRef.current && arrowRef.current) {
+      gsap.to(shoeCardRef.current, {
+        x: "-45",
+        duration: 0.3,
+      });
+      gsap.set(circleCoverRef.current, {
+        right: "0%",
+      });
+      gsap.set(arrowRef.current, {
+        color: "white",
+      });
+    }
+  }
+  function handelHoverLeave() {
+    if (shoeCardRef.current && circleCoverRef.current && arrowRef.current) {
+      gsap.to(shoeCardRef.current, {
+        x: 0,
+        duration: 0.3,
+      });
+      gsap.set(circleCoverRef.current, {
+        right: "100%",
+      });
+      gsap.set(arrowRef.current, {
+        color: "black",
+      });
+    }
+  }
   return (
-    <div className="bg-white h-[480px] w-[450px] rounded-[20px] overflow-hidden z-[22]">
+    <div
+      className="bg-white h-[480px] w-[450px] rounded-[20px] overflow-hidden z-[22]"
+      onMouseEnter={handelHoverEnter}
+      onMouseLeave={handelHoverLeave}
+    >
       <div className="h-[70%]  ">
-        <img src={shoe} className="relative right-[-10%]" />
+        <img src={shoe} className="relative right-[-10%]" ref={shoeCardRef} />
       </div>
       <div className="h-[30%]  flex justify-between items-center px-[1.7rem] text-black">
         <div className="text-[1.2rem] leading-[100%]">
           <p>{title}</p>
           <p>{desc}</p>
         </div>
-        <div className="border border-black border-opacity-[.3] p-[1rem] rounded-[50%] h-[60px] w-[60px] flex items-center justify-center">
-          <GoArrowRight className="text-black text-[1.2rem]" />
+        <div className="overflow-hidden relative border border-black border-opacity-[.3] p-[1rem] rounded-[50%] h-[60px] w-[60px] flex items-center justify-center">
+          <div
+            ref={circleCoverRef}
+            className="bg-[#eb244b] h-[60px] w-[100px] rounded-r-[35px] absolute z-[30] right-[100%] transition-all ease-linear"
+          ></div>
+          <span className="text-black z-[31]" ref={arrowRef}>
+            <GoArrowRight className=" text-[1.2rem] " />
+          </span>
         </div>
       </div>
     </div>
